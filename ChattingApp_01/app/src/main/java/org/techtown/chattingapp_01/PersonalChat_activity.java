be10befbcd91ae3c329bea220e78c2ef7c6f5fa3
iamwotjr99 <iamwotjr@gmail.com>
2021-07-17 20:34:24 +0900

    [추가] 채팅방 내부에 draw 할 수 있는 ui, 기능

---
+++ ChattingApp_01/app/src/main/java/org/techtown/chattingapp_01/PersonalChat_activity.java
@@ -0,0 +1,74 @@
+package org.techtown.chattingapp_01;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import org.jetbrains.annotations.NotNull;
+
+public class PersonalChat_activity extends AppCompatActivity {
+
+    private DrawerLayout mainPersonalChatLayout;
+    private View drawerMenu;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_personalchat);
+
+        mainPersonalChatLayout = findViewById(R.id.mainPersonalChat_layout);
+        drawerMenu = findViewById(R.id.drawer_menu);
+
+        ImageButton imgButton_menu = findViewById(R.id.imgButton_menu);
+        imgButton_menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainPersonalChatLayout.openDrawer(drawerMenu);
+            }
+        });
+
+        mainPersonalChatLayout.setDrawerListener(drawerListener);
+        drawerMenu.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return true;
+            }
+        });
+
+
+    }
+
+    DrawerLayout.DrawerListener drawerListener = new DrawerLayout.DrawerListener() {
+        @Override
+        public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+
+        }
+
+        @Override
+        public void onDrawerOpened(@NonNull View drawerView) {
+
+        }
+
+        @Override
+        public void onDrawerClosed(@NonNull View drawerView) {
+
+        }
+
+        @Override
+        public void onDrawerStateChanged(int newState) {
+
+        }
+    };
+}
