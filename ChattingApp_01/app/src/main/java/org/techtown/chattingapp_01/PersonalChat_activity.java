c86cf0fd02d7b2cf7190aa7f21da92dab4a97e9b
iamwotjr99 <iamwotjr@gmail.com>
2021-07-21 20:42:00 +0900

    [추가] Firebase realtimeDB 연결, 프로젝트랑 직접적인 연결 성공

--- ChattingApp_01/app/src/main/java/org/techtown/chattingapp_01/PersonalChat_activity.java
+++ ChattingApp_01/app/src/main/java/org/techtown/chattingapp_01/PersonalChat_activity.java
@@ -16,6 +16,9 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import org.jetbrains.annotations.NotNull;
 
 public class PersonalChat_activity extends AppCompatActivity {
@@ -47,6 +50,11 @@ public class PersonalChat_activity extends AppCompatActivity {
                 return true;
             }
         });
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
 
 
     }
