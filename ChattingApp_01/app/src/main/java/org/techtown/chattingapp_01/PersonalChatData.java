543205bf771fd2565c0f31378f27ff42fbc9401d
iamwotjr99 <iamwotjr@gmail.com>
2021-07-21 18:27:35 +0900

    [추가] PersonalChatAdapter

--- ChattingApp_01/app/src/main/java/org/techtown/chattingapp_01/PersonalChatData.java
+++ ChattingApp_01/app/src/main/java/org/techtown/chattingapp_01/PersonalChatData.java
@@ -1,8 +1,28 @@
 package org.techtown.chattingapp_01;
 
+import android.net.Uri;
+
 public class PersonalChatData {
     private String nickname;
     private String msg;
+    private String profile;
+    private String time;
+
+    public String getProfile() {
+        return profile;
+    }
+
+    public void setProfile(String profile) {
+        this.profile = profile;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
 
     public String getNickname() {
         return nickname;
